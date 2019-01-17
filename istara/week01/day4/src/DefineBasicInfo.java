3b2e98c4aa92dd29054d4cf18d6f39d5ac03d8ba
IstvanA <istvan.aranyi2@gmail.com>
2019-01-17 15:19:28 +0100

    DefineBasicInfo

---
+++ istara/week01/day4/src/DefineBasicInfo.java
@@ -0,0 +1,20 @@
+import sun.font.Script;
+
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        String name = "István Aranyi";
+        System.out.println(name);
+        int age = 30;
+        System.out.println(age);
+        double height = 1.87;
+        System.out.println(height);
+        boolean married = false;
+        System.out.println(married);
+    }
+}
