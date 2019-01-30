a21d8411e52543f4c40e84c3e6cb44cd2c274c8a
IstvanA <istvan.aranyi2@gmail.com>
2019-01-30 12:27:59 +0100

    Sharpie

---
+++ istara/week03/day3/src/sharpie/Sharpie.java
@@ -0,0 +1,14 @@
+package sharpie;
+
+public class Sharpie {
+    String color;
+    float width;
+    float inkAmount = 100;
+    public Sharpie(String color, float width) {
+        this.color = color;
+        this.width = width;
+    }
+    public int use() {
+        inkAmount = inkAmount - 1;
+    }
+}
