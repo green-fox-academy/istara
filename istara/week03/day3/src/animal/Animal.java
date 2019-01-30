ed1c494c647fef5d709997c0efa862329d27a2e7
IstvanA <istvan.aranyi2@gmail.com>
2019-01-30 13:18:07 +0100

    Animal

--- istara/week03/day3/src/animal/Animal.java
+++ istara/week03/day3/src/animal/Animal.java
@@ -3,13 +3,15 @@ package animal;
 public class Animal {
     int hunger = 50;
     int thirst = 50;
-    public int eat() {
+    public void eat() {
         hunger = hunger - 1;
     }
-    public int drink() {
+
+    public void drink() {
         thirst = thirst - 1;
     }
-    public int play() {
+
+    public void play() {
         hunger = hunger + 1;
         thirst = thirst + 1;
     }
