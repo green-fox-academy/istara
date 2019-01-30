eb543091fc13d1e61299cdcb3a629e56cc2e19ce
IstvanA <istvan.aranyi2@gmail.com>
2019-01-30 11:32:30 +0100

    PostIt

---
+++ istara/week03/day3/src/classes/Classes.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class Classes {
+    public static void main(String[] args) {
+        PostIt example1 = new PostIt("orange","Idea 1" ,"blue");
+        PostIt example2 = new PostIt("pink","Awesome" ,"black");
+        PostIt example3 = new PostIt("yellow","Superb!" ,"green");
+    }
+}
