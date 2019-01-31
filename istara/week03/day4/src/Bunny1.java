15ef617f7543d17910d37d867eaaa1ae524a9222
IstvanA <istvan.aranyi2@gmail.com>
2019-01-31 16:30:42 +0100

    Bunny1

--- istara/week03/day4/src/Bunny1.java
+++ istara/week03/day4/src/Bunny1.java
@@ -1,6 +1,5 @@
 // We have a number of bunnies and each bunny has two big floppy ears.
-// We want to compute the total number of ears across all the bunnies recursively
-// (without loops or multiplication).
+// We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
 
 public class Bunny1 {
     public static void main(String[] args) {
