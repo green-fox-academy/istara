ec8b1b60d35456d6e4900be3df148f253b286a17
IstvanA <istvan.aranyi2@gmail.com>
2019-02-01 08:05:19 +0100

    Bunny2

---
+++ istara/week03/day4/src/Bunny2.java
@@ -0,0 +1,25 @@
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+// (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+// have 3 ears, because they each have a raised foot. Recursively return the
+// number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+
+public class Bunny2 {
+    public static void main(String[] args) {
+        int bunnyNium = 0;
+        System.out.println(bunnyResult(5));
+    }
+    public static int bunnyResult(int n) {
+        int oddEars = 2;
+        int evenEars = 3;
+        int totalEars = 0;
+        if (n > 0) {
+            if (n % 2 == 1) {
+                totalEars = oddEars + bunnyResult(n - 1);
+            } else {
+                totalEars = evenEars + bunnyResult(n - 1);
+            }
+        }
+        return totalEars;
+    }
+}
