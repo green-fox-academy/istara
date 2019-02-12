c605941a0776ddcd490ba1f2d5dec1e796918dc8
IstvanA <istvan.aranyi2@gmail.com>
2019-02-12 11:19:32 +0100

    Zoo

--- istara/week04/day3/src/main/java/animals/Bird.java
+++ istara/week04/day3/src/main/java/animals/Bird.java
@@ -1,6 +1,6 @@
 package main.java.animals;
 
-public class Bird extends Animal{
+public class Bird extends Animal implements Flyable{
 
     public Bird(String name) {
         super(name);
@@ -10,5 +10,20 @@ public class Bird extends Animal{
     public String breed() {
         String breedType = "laying eggs.";
         return breedType;
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void takeoff() {
+
     }
 }
